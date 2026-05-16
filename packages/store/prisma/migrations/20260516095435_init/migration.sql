c32aecbdf098b7fa628f9549405a80e862ab74c8
Pankeet <pankeet04@gmail.com>
2026-05-16 15:47:11 +0530

    Fix: prisma generated files

--- packages/store/prisma/migrations/20260516095435_init/migration.sql
+++ packages/store/prisma/migrations/20260516095435_init/migration.sql
@@ -5,7 +5,8 @@ CREATE TYPE "WebsiteStatus" AS ENUM ('Up', 'Down', 'Unknown');
 CREATE TABLE "Website" (
     "id" TEXT NOT NULL,
     "url" TEXT NOT NULL,
-    "timeAdded" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+    "user_id" TEXT NOT NULL,
 
     CONSTRAINT "Website_pkey" PRIMARY KEY ("id")
 );
@@ -25,12 +26,30 @@ CREATE TABLE "WebsiteTick" (
     "status" "WebsiteStatus" NOT NULL,
     "region_id" TEXT NOT NULL,
     "website_id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "WebsiteTick_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "avatar" TEXT,
+    "password" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "Website_url_key" ON "Website"("url");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Website" ADD CONSTRAINT "Website_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "WebsiteTick" ADD CONSTRAINT "WebsiteTick_region_id_fkey" FOREIGN KEY ("region_id") REFERENCES "Region"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
